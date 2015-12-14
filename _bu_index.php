87e95dc7956976e1339a654817632cc9fcff0c97
Joost Brommert <info@webbiker.nl>
2015-12-14 20:30:28 +0100

    Backup of index file

---
+++ _bu_index.php
@@ -0,0 +1,55 @@
+<?php include_once('assets/_bu_includes/inc.header.php'); ?>
+<div class="bikers">
+        <div class="ripborder">
+                <header>
+                    <?php include_once('assets/_bu_includes/inc.navigation.php'); ?>
+                    <?php include_once('assets/_bu_includes/inc.caroussel.php'); ?>
+                    <?php include_once('assets/_bu_includes/inc.clouds.php'); ?>
+                </header>
+                <div class="mainContent">
+                        <div class="services clearfix">
+                                <section>
+                                        <h1><span></span>Internet</h1>
+                                        <p>Donec dapibus lacinia mi et sodales. Quisque hendrerit leo eu eros vehicula aliquam a in tortor. Nunc elementum, purus ut sodales rutrum, mauris ante.</p>
+                                        <p><a class="button" href="javascript:;">Lees meer</a></p>
+                                </section>
+                                <section>
+                                        <h1><span></span>Hosting</h1>
+                                        <p>Donec dapibus lacinia mi et sodales. Quisque hendrerit leo eu eros vehicula aliquam a in tortor. Nunc elementum, purus ut sodales rutrum, mauris ante.</p>
+                                        <p><a class="button" href="javascript:;">Lees meer</a></p>
+                                </section>
+                                <section>
+                                        <h1><span></span>Multimedia</h1>
+                                        <p>Donec dapibus lacinia mi et sodales. Quisque hendrerit leo eu eros vehicula aliquam a in tortor. Nunc elementum, purus ut sodales rutrum, mauris ante.</p>
+                                        <p><a class="button" href="javascript:;">Lees meer</a></p>
+                                </section>
+                                <section class="last">
+                                        <h1><span></span>Prepress</h1>
+                                        <p>Donec dapibus lacinia mi et sodales. Quisque hendrerit leo eu eros vehicula aliquam a in tortor. Nunc elementum, purus ut sodales rutrum, mauris ante.</p>
+                                        <p><a class="button" href="javascript:;">Lees meer</a></p>
+                                </section>
+                        </div>
+
+                        <section class="about clearfix">
+                                <h1><img src="/assets/img/homepage/about_biker.png" align="right">About Webbiker</h1>
+                                <p>Pellentesque lacinia semper blandit. Suspendisse sit amet commodo nisi. <br>Fusce ipsum ipsum, cursus eu blandit ac, tincidunt non mauris. Nulla blandit <br>accumsan erat, vel vehicula ante lacinia consequat.</p>
+                                <p>Vestibulum eget neque tortor. Praesent neque quam, pharetra ut luctus in, <br>vehicula euismod ipsum. Sed quis dolor vitae nisl auctor sagittis quis nec <br>lorem. In hac habitasse platea dictumst. Integer a ante eget felis aliquet <br>volutpat a sit amet urna.</p>
+                                <a href="javascript:;" class="bigbutton"><span>Lees meer</span><h2>Over de Webbiker</h2></a>
+                        </section>
+                        <section class="contact clearfix">
+                                <h1>GEEF ME WAT<br>BENZINE!</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisi ipsum, consectetur in tempus ut, posuere eu sapien.</p>
+                                <p>Quisque fringilla velit ac<br>mauris tincidunt ac<br>blandit quam <br>adipiscing. Cras quis <br>blandit felis. Aliquam <br>erat volutpat.</p>
+                                <p>Vivamus volutpat <br>euismod ligula, id <br>egestas risus <br>malesuada sed.</p>
+                                <a href="javascript:;" class="bigbutton"><span>Contact</span><h2>Laat wat van je horen</h2></a>
+                                <img src="/assets/img/homepage/contact_gaspump.png">
+                        </section>
+                        <section class="twitter clearfix">
+                                <h1>@WebbikerNL</h1>
+                                <time datetime="2012-05-01">1 Mei 2012</time>
+                                <p><img src="/assets/img/homepage/twitter_quote_open.png" class="qopen"><a href="javascript:;">@hans2103</a> Laatste versie van Mage-World gedownload, cache gecleared, maar geen toolbar. Deze: <a href="javascript:;">http://goo.gl/CKYEU</a> lukte wel op CE en EE.<img src="/assets/img/homepage/twitter_quote_close.png" class="qclose"></p>
+                        </section>
+                </div>
+        </div>
+</div>
+<?php include_once('assets/_bu_includes/inc.footer.php'); ?>
