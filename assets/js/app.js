7db8bd4632f138da0fe72c9fbb28099561ee218e
Webbiker <info@webbiker.nl>
2017-01-19 16:37:48 +0100

    Fixed front-end header navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,17 @@
 /* -------------------------
-   Global functions
+   Sticky header function
    ------------------------- */
+var stickyHeader = function(){
+	// behaviour of the main navigation on scroll
+	var height = $(window).scrollTop();
+	if(height  > 40) {
+		$('header').addClass('header--sticky');
+	} else {
+		$('header').removeClass('header--sticky');
+	}
+}
 
-	$(document).ready(function(){
-
-
-
-		$(window).scroll(function() {
-			// console.log('wrapper is scrolling');
-
-			// behaviour of the main navigation on scroll
-			var wrapperPos = $('.wrapper').scrollTop();
-			// console.log('height: '+ wrapperPos);
-
-
-			if(wrapperPos  > 40) {
-				$('.header').addClass('header--sticky');
-			} else {
-				$('.header').removeClass('header--sticky');
-			}
-
-		});
-	});
-
-	$(window).scroll(function(){
-		var wrapperPos = $('p').scrollTop();
-		console.log('scrolling: '+wrapperPos);
-	});
-
+$(document).ready(function(){
+	$(window).scroll(stickyHeader);
+});
+stickyHeader();
--- index.php
+++ index.php
@@ -47,10 +47,12 @@
 					<ul>
 						<li><a href="javascript:;" class="active">Over Webbiker</a></li>
 						<li><a href="javascript:;">Portfolio</a></li>
+						<li><a href="/" class="logo"><img src="/assets/img/header/logo.svg" alt="Webbiker logo"></a></li>
 						<li><a href="javascript:;">Diensten</a></li>
 						<li><a href="javascript:;">Contact</a></li>
 					</ul>
-					<a href="/" class="logo">
+<!-- 					<a href="/" class="logo">
+						<img src="/assets/img/header/logo.svg" alt="Webbiker logo">
 						<svg viewBox="0 0 229 232" class="logo__shield">
 							<use xlink:href="/assets/img/gui/sprite.svg#logo-shield"></use>
 						</svg>
@@ -62,9 +64,8 @@
 						</svg>
 						<svg viewBox="0 0 229 232" class="logo__shadow">
 							<use xlink:href="/assets/img/gui/sprite.svg#logo-shadow"></use>
-					</a>
+					</a> -->
 				</nav>
-
 				<div class="clouds clouds__horizon"></div>
 				<div class="clouds clouds__cloudone"></div>
 				<div class="clouds clouds__cloudtwo"></div>
@@ -87,6 +88,7 @@
 					<p>Curabitur laoreet pulvinar dui, vitae interdum ligula tincidunt at. Cras rhoncus velit urna, vel eleifend orci accumsan ut. Aliquam eget consequat massa, eget vulputate elit. Pellentesque justo diam, tempor ut erat id, congue aliquet tellus. Etiam congue dignissim dignissim. Sed quis nibh in sem faucibus interdum ut vel nulla. Fusce porttitor ornare euismod. Vestibulum dolor metus, convallis non velit sed, ullamcorper placerat ante.</p>
 					<p>Maecenas eros ante, dapibus sed ipsum sit amet, maximus maximus ex. Nullam odio elit, venenatis id dictum nec, consectetur eu metus. Donec in orci sit amet eros faucibus porta sagittis nec metus. Nullam quis suscipit lectus, nec iaculis turpis. Curabitur euismod eros sit amet magna dignissim, id pellentesque est egestas. Nam sit amet suscipit ex, eget consequat orci. Aliquam erat volutpat. Aliquam erat volutpat. Proin molestie, urna eu dignissim pretium, diam felis tincidunt nisl, eu porttitor sapien velit non magna. Proin et sagittis magna. Aenean quis metus eu neque ultricies ornare nec ut lacus. Phasellus cursus arcu sollicitudin, molestie ex id, laoreet tellus. Mauris vel molestie eros.</p>
 					<h2>Lorem ipsum dolor sit amet</h2>
+					<!-- <p><svg xmlns:xlink="http://www.w3.org/1999/xlink"><use xlink:href="/assets/img/gui/sprite.svg#logo" title="Webbiker logo"></use></svg></p> -->
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut molestias, voluptatum cumque aut nostrum, nisi sint voluptates illo repellat quis voluptate iure quos dolorum ullam incidunt vitae quisquam tempore possimus, et sequi perspiciatis aliquid omnis distinctio deleniti. Sed, dicta porro neque ab enim consequuntur assumenda tempore vitae fugit nam inventore qui, placeat distinctio impedit labore. Cupiditate quia deleniti aperiam quaerat voluptatem at temporibus ullam! Ex modi mollitia natus at earum assumenda nulla quam eum. </p>
 					<h3>Lorem ipsum dolor sit amet</h3>
 					<p>Voluptates esse, maiores saepe quis assumenda, eum odio atque consequatur blanditiis minima beatae similique placeat quas velit, quos ad provident commodi dolor eos quibusdam dolorum debitis fuga? Placeat quod, odit fuga quo sequi assumenda magni officia laudantium quam corporis, amet ullam tempora repellat porro architecto nemo eaque nisi laboriosam nam.</p>
